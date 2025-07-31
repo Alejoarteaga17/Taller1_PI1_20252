6d5ed492bf9151648c533344d2b72e72adfa6efe
Alejo Arteaga <alejoarte123@icloud.com>
2025-07-31 11:00:34 -0500

    Cambiando funcion home

--- movie/views.py
+++ movie/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name': 'Greg Lim'})
+    return render(request, 'home.html', {'name': 'Alejandro Arteaga H'})
 def about(request):
     return HttpResponse('<h1>Welcome 2 About Page</h1>')
 
